6d09c0a32b50d1aaf9df69d7c08a797bb2d0b43c
Muhammed <muhammed@sendme.ng>
2022-08-04 17:17:38 +0100

    feat: verify payment url

--- core/urls.py
+++ core/urls.py
@@ -2,4 +2,5 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('', views.initiate_payment, name='initiate-payment'),
+    path('<str:ref>/', views.verify_payment, name='verify-payment'),
     ]
--- core/views.py
+++ core/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from . import forms
+from django.conf import settings
 # Create your views here.
 
 
@@ -9,7 +10,7 @@ def initiate_payment(request):
         if payment_form.is_valid():
             payment = payment_form.save()
 
-            render(request, 'make_payment.html', {'payment': payment})
+            render(request, 'make_payment.html', {'payment': payment, 'paystack_public_key': settings.PAYSTACK_PUBLIC_KEY})
 
     else:
         payment_form = forms.PaymentForm()
