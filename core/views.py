4d2023ce356fc01e7f5e25112793592dc24a7920
Muhammed <muhammed@sendme.ng>
2022-08-04 17:20:27 +0100

    feat: verify payment

--- core/views.py
+++ core/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, redirect, render
 from . import forms
 from django.conf import settings
+from .models import Payment
 # Create your views here.
 
 
@@ -16,3 +17,9 @@ def initiate_payment(request):
         payment_form = forms.PaymentForm()
 
     return render(request, 'initiate_payment.html', {'payment_form': payment_form})
+
+
+def verify_payment(request, ref):
+    payment = get_object_or_404(Payment, ref=ref)
+    verified = payment.verify_payment()
+    return redirect('initiate-payment')
