0cb6e8dc3ecda428d1996c46c061f9b94edaae08
Muhammed <muhammed@sendme.ng>
2022-08-04 15:47:36 +0100

    feat: create initiate_payment view

--- core/views.py
+++ core/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
-
+from . import forms
 # Create your views here.
+
+
+def initiate_payment(request):
+    if request.method == 'POST':
+        payment_form = forms.PaymentForm(request.POST)
+        if payment_form.is_valid():
+            payment = payment_form.save()
+
+            render(request, 'make_payment.html', {'payment': payment})
+
+    else:
+        payment_form = forms.PaymentForm()
+
+    return render(request, 'initiate_payment', {'payment_form': payment_form})
